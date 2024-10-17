2e33d8f719cb8222124ab9347d71c01aff7a2cf2
MsSuraj001 <milukumar225@gmail.com>
2024-10-17 13:50:56 +0530

    create a project to use the vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import "./app.css"
+
+function App() {
+
+  return (
+    <>
+     <h1>this is vite react </h1>
+     <h1>this is the vite react prectice</h1>
+    </>
+  )
+}
+
+export default App
